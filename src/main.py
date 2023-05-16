a3b9082a67144ed933c439646538abd6de6c3ff6
dduwsc81 <duc61235@gmail.com>
2023-05-16 16:52:22 +0700

    Update allow CORS

--- src/main.py
+++ src/main.py
@@ -5,8 +5,23 @@ from passlib.context import CryptContext
 
 from api.api import api_router
 from security.security import check_token
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost:3000",
+    "http://localhost:8080",
+]
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(api_router)
 
