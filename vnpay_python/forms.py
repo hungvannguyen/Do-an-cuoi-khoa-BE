5d936123003bffbc38fe244dd22d21a6937310a5
dduwsc81 <duc61235@gmail.com>
2023-06-24 01:40:03 +0700

    update API generate VNPAY links

---
+++ vnpay_python/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from typing import Optional
+
+from pydantic import BaseModel
+from schemas.Default import Config
+
+
+class PaymentForm(BaseModel, Config):
+    # order_id: Optional[str]
+    # order_type: Optional[str]
+    amount: Optional[float]
+    order_info: Optional[str]
+    bank_code: Optional[str]
+    language: Optional[str]
